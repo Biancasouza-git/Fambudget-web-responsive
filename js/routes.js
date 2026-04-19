0dc19d97eba4a63a08374764fea677ab6be83bb0
Beatriz Dias <beatrizzbarbozaa88@gmail.com>
2026-04-19 16:14:59 -0300

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const { login, register } = require("./auth");
+const { login, register, refreshToken } = require("./auth");
 
 router.post("/auth/login", login);
 router.post("/auth/register", register);
+router.post("/auth/refresh", refreshToken);
 
 module.exports = router;
