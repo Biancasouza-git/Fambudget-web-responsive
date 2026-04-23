a9abd0b745c3678f0ec96ef97b33a064b0000224
Bianca Souza <bixrtsouza@gmail.com>
2026-04-23 14:28:09 -0300

    Create orcamentos.js

---
+++ js/orcamentos.js
@@ -0,0 +1,36 @@
+function criarOrcamento(nome, total, gasto) {
+  let lista = document.getElementById("lista-orcamentos");
+
+  let porcentagem = (gasto / total) * 100;
+
+  let cor = "#2e7d32";
+  if (porcentagem > 80) cor = "#f9a825";
+  if (porcentagem > 95) cor = "#d32f2f";
+
+  let card = document.createElement("div");
+  card.classList.add("card-orcamento");
+
+  card.innerHTML = `
+    <div class="topo-orcamento">
+      <div class="info">
+        <div class="icone">💰</div>
+        <strong>${nome}</strong>
+      </div>
+
+      <span>R$ ${total.toFixed(2)}</span>
+    </div>
+
+    <div class="barra">
+      <div class="progresso" style="width:${porcentagem}%"></div>
+    </div>
+
+    <div class="topo-orcamento">
+      <span>R$ ${(total - gasto).toFixed(2)}</span>
+      <span>${porcentagem.toFixed(0)}% do limite</span>
+    </div>
+  `;
+
+  lista.appendChild(card);
+}
+
+/* CRIAR FORMULARIO ORÇAMENTO */
