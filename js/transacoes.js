9d10fb51da09eaa4066190b2610f8aa85e4f5e06
Bianca Souza <bixrtsouza@gmail.com>
2026-04-24 14:56:09 -0300

    Update 2 transacoes.js

--- js/transacoes.js
+++ js/transacoes.js
@@ -1,3 +1,23 @@
+console.log("TRANSACOES.JS OK");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  carregarUsuario();
+  await carregarDadosFinanceiros();
+});
+
+function carregarUsuario() {
+  const nome = localStorage.getItem("nomeUsuario");
+
+  console.log("NOME:", nome);
+
+  if (!nome) return;
+
+  document.getElementById("nome-usuario").textContent = nome;
+  document.getElementById("avatar").textContent = nome.charAt(0).toUpperCase();
+}
+
+
+/* MODALLLLL */
 function abrirModal() {
   document.getElementById("modal").style.display = "flex";
 }
